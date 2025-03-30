8e39f111cc13a7481b0b17cacc87846900d7e532
rajeshmedipally <115456612+rajeshmedipally@users.noreply.github.com>
2025-03-31 01:17:06 +0530

    Update app.js

--- app.js
+++ app.js
@@ -13,10 +13,12 @@ todoForm.addEventListener('submit', function(e){
 
 function addTodo(){
     const todoText = todoInput.value.trim();
+    const priority = document.querySelector('input[name="priority"]:checked').value;
     if(todoText.length > 0){
         const todoObject = {
             text: todoText,
-            completed: false
+            completed: false,
+            priority: priority
         }
         allTodos.push(todoObject);
         updateTodoList();
@@ -37,7 +39,8 @@ function createTodoItem(todo, todoIndex){
     const todoId = "todo-" + todoIndex;
     const todoLI = document.createElement("li");
     const todoText = todo.text;
-    todoLI.className = "todo";
+    const priorityClass = todo.priority.toLowerCase();
+    todoLI.className = `todo ${priorityClass}`;
     todoLI.innerHTML = `
     <input type="checkbox" id="${todoId}">
                 <label for="${todoId}" class="custom-checkbox">
@@ -45,7 +48,7 @@ function createTodoItem(todo, todoIndex){
                     </svg>
                 </label>
                 <label for="${todoId}" class="todo-text">
-                    ${todoText}
+                    ${todoText} 
                 </label>
                 <button class="delete-button">
                     <svg fill="--secondary-color" xmlns="http://www.w3.org/2000/svg" height="24px" viewBox="0 -960 960 960" width="24px" fill="#e3e3e3"><path d="M280-120q-33 0-56.5-23.5T200-200v-520h-40v-80h200v-40h240v40h200v80h-40v520q0 33-23.5 56.5T680-120H280Zm400-600H280v520h400v-520ZM360-280h80v-360h-80v360Zm160 0h80v-360h-80v360ZM280-720v520-520Z"/>
